50895b9132b744b531740dde6f3a201babce2dd0
TechStackInb <coding@inboundsys.com>
2024-08-28 16:41:10 +0530

    patch updated

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 const options = {
   origin: 'https://ackuitypreview.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
