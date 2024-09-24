2f582cc97e6c5f2ae012f8a1eac5931fa3c92bd3
TechStackInb <coding@inboundsys.com>
2024-09-24 11:04:01 +0530

    text2SQL

--- src/models/PolicyManagerText2SQL.js
+++ src/models/PolicyManagerText2SQL.js
@@ -1,5 +1,38 @@
 const mongoose = require('mongoose');
 
+const plusDataArray = new mongoose.Schema({
+  ONname: {
+    type: String,
+    enum: ['Opportunity Name', 'Account Name', 'Amount', 'Age'],
+  },
+  ONprivacyFilteringAction: {
+    type: String,
+    enum: ['Anonymize', 'Tokenize', 'None', 'De-Identification'],
+  },
+  ONprivacyFilteringTransformValue: {
+    type: String,
+  },
+  ONattributeFilteringAttribute: {
+    type: String,
+    enum: ['Department', 'Location'],
+  },
+  ONattributeFilteringValue: {
+    type: String,
+    enum: ['Asia', 'North America'],
+  },
+  ONattributeFilteringAction: {
+    type: String,
+    enum: ['Allow', 'Redact'],
+  },
+  ONattributeFilteringTransformationValue: {
+    type: String,
+  },
+  rowLevelFilteringBasedonValue: {
+    type: String,
+    enum: ['XYZ Corp', 'ABC Corp', 'DEF Corp', 'MNO Corp'],
+  },
+});
+
 const text2SQLSchema = new mongoose.Schema(
   {
     policyName: {
@@ -61,35 +94,8 @@ const text2SQLSchema = new mongoose.Schema(
       type: String,
       enum: ['TABLE1', 'TABLE2', 'TABLE3'],
     },
-    ONname: {
-      type: String,
-      enum: ['Opportunity Name', 'Account Name', 'Amount', 'Age'],
-    },
-    ONprivacyFilteringAction: {
-      type: String,
-      enum: ['Anonymize', 'Tokenize', 'None', 'De-Identification'],
-    },
-    ONprivacyFilteringTransformValue: {
-      type: String,
-    },
-    ONattributeFilteringAttribute: {
-      type: String,
-      enum: ['Department', 'Location'],
-    },
-    ONattributeFilteringValue: {
-      type: String,
-      enum: ['Asia', 'North America'],
-    },
-    ONattributeFilteringAction: {
-      type: String,
-      enum: ['Allow', 'Redact'],
-    },
-    ONattributeFilteringTransformationValue: {
-      type: String,
-    },
-    rowLevelFilteringBasedonValue: {
-      type: String,
-      enum: ['XYZ Corp', 'ABC Corp', 'DEF Corp', 'MNO Corp'],
+    plusData: {
+      type: [plusDataArray],
     },
   },
   {
